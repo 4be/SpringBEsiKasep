e116dc73fd0c052c1308facf5caae4a57cdcf02d
Rian Netta <jaderizan@gmail.com>
2021-07-29 02:18:07 +0000

    Revert "Merge branch 'springadi' into 'main'"
    This reverts merge request !2

--- src/main/java/com/sekoding/example/demo/model/repos/ClockinRepo.java
+++
@@ -1,7 +0,0 @@
-package com.sekoding.example.demo.model.repos;
-
-import com.sekoding.example.demo.model.entity.Clockin;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClockinRepo extends CrudRepository<Clockin,Long> {
-}
