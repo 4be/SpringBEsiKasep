d3bd6f4303016e84d3ce5e5878a643f79c141029
abe <gakpernahlupa@gmail.com>
2021-07-30 21:16:07 +0700

    Fix service Clockin upload gambar dari flutter ke server vm

--- src/main/java/com/sekoding/example/demo/model/entity/Clock.java
+++ src/main/java/com/sekoding/example/demo/model/entity/Clock.java
@@ -33,10 +33,10 @@ public @Data class Clock implements Serializable{
         @Column(length = 100)
         private String location_clockout;
 
-        @Column(length = 100)
+        @Column(length = 300)
         private String url_foto_clockin;
 
-        @Column(length = 100)
+        @Column(length = 300)
         private String url_foto_clockout;
 
         @Column(length = 10)
