b1dfbc289c67b45d5b81fe3885c8622c231fb87a
fathurrj <foltak98@gmail.com>
2021-08-13 13:49:57 +0700

    Penambahan delete V1

--- src/main/java/com/sekoding/example/demo/repository/UserRepository.java
+++ src/main/java/com/sekoding/example/demo/repository/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
-//@Repository
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsernameOrEmail(String username, String email);
