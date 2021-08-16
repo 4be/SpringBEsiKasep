d80a98ecdfb799581db0dd1a90229b13edfcad28
jordyk0906 <jordykristianata@gmail.com>
2021-08-16 11:49:10 +0700

    .

--- src/test/java/com/sekoding/example/demo/controller/KeteranganControllerTest.java
+++ src/test/java/com/sekoding/example/demo/controller/KeteranganControllerTest.java
@@ -3,34 +3,46 @@ package com.sekoding.example.demo.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sekoding.example.demo.model.entity.Keterangan;
+import com.sekoding.example.demo.model.repos.KeteranganRepo;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpMethod;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.http.RequestEntity.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class KeteranganControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
-
     ObjectMapper objectMapper = new ObjectMapper();
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private KeteranganRepo ketRepo;
+
     @Test
-    public void testKeteranganMustNotBeBlank() throws JsonProcessingException,  Exception {
-        Keterangan ket = new Keterangan();
+    public void testListKeterangan() throws Exception {
+        List<Keterangan> listKet = new ArrayList();
+        listKet.add(new Keterangan());
+        listKet.add(new Keterangan());
+        Mockito.when(ketRepo.findAll()).thenReturn(listKet);
         String url = "/api/keterangan/list";
-        mockMvc.perform(post(url)
-            .contentType("application/json")
-            .content(objectMapper)
-        );
+        mockMvc.perform(get(url)).andExpect(status().isOk());
     }
 }
